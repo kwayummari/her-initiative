f1ea71a5c8b37b07228ee90828ec28b92f10c876
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-04 15:49:44 +0300

    fixed

--- src/screens/recognition/recognitions.js
+++ src/screens/recognition/recognitions.js
@@ -1,22 +1,15 @@
 import React from "react";
-import './styles.css'
+import './styles.css';
 import { Grid, useMediaQuery } from "@mui/material";
 import { Box, Stepper, Step, StepLabel, StepContent, Button, Paper } from '@mui/material';
 
 const steps = [
     {
-        label: 'October 14, 2021',
+        label: '2024',
         description: (
             <div>
-                Panda Digital was recognized by the Ministry of Health, Community Development, Gender, Elders, and Children for being the first Swahili-focused digital platform dedicated to girls’ education in Tanzania.
-            </div>
-        ),
-    },
-    {
-        label: 'April 27, 2022',
-        description: (
-            <div>
-                Panda Digital was acknowledged by the +1 Global Fund under the Roddenberry Foundation, receiving a USD 12,000 prize for its substantial contribution to advancing education in Africa through digital technology.
+                <h1>2024 Global Citizen Prize 🏆</h1>
+                <img src='/photos/global.jpeg' alt='Global Citizen Prize' className='responsiveImage' />
             </div>
         ),
     },
@@ -29,11 +22,18 @@ const steps = [
         ),
     },
     {
-        label: '2024',
+        label: 'April 27, 2022',
         description: (
             <div>
-                <h1>2024 Global Citizen Prize 🏆</h1>
-                <img src='/photos/global.jpeg' alt='Global Citizen Prize' className='responsiveImage' />
+                Panda Digital was acknowledged by the +1 Global Fund under the Roddenberry Foundation, receiving a USD 12,000 prize for its substantial contribution to advancing education in Africa through digital technology.
+            </div>
+        ),
+    },
+    {
+        label: 'October 14, 2021',
+        description: (
+            <div>
+                Panda Digital was recognized by the Ministry of Health, Community Development, Gender, Elders, and Children for being the first Swahili-focused digital platform dedicated to girls’ education in Tanzania.
             </div>
         ),
     },
@@ -69,7 +69,7 @@ function RecognitionsPart() {
                                 <Step key={step.label}>
                                     <StepLabel
                                         optional={
-                                            index === 3 ? (
+                                            index === 0 ? (
                                                 <p style={{ fontSize: '20px', fontWeight: '700', color: '#633e98' }}>Last step</p>
                                             ) : null
                                         }
