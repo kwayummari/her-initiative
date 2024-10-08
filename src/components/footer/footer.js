f1ec62ff566137e8fa32176c21b24c814f26865c
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-08 14:04:26 +0300

    deleted page module.css

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,14 +11,11 @@ function BottomFooter() {
                 existingWidget.parentNode.removeChild(existingWidget);
             }
 
-            // Create and append the widget elements
             const widgetImg = document.createElement("img");
             widgetImg.src = '/photos/ngo.png';
-            // http://www.ngosource.org/sites/default/files/ngos_ed_on_file_widget.png
             widgetImg.alt = 'NGOsource Equivalency Determination on File';
-            widgetImg.style.marginTop = '80px';
-            widgetImg.style.marginRight = '10px';
-            // widgetImg.style.marginRight = '30px';
+            widgetImg.style.marginTop = '20px';
+            // widgetImg.style.marginRight = '10px';
 
             const widgetLink = document.createElement("a");
             const theUrl = 'http://www.ngosource.org/about-equivalency-determination-on-file-badge';
@@ -30,7 +27,7 @@ function BottomFooter() {
             widgetContainer.id = 'ngos-ed-on-file-widget-script-17336c1f-917d-492b-bc53-225c95e103da';
             widgetContainer.appendChild(widgetLink);
 
-            const footerContainer = document.querySelector('.footerContainer');
+            const footerContainer = document.querySelector('.donation');
             footerContainer.appendChild(widgetContainer);
         };
 
@@ -56,44 +53,60 @@ function BottomFooter() {
 
     return (
         <div className='footer' style={{ backgroundColor: '#212121' }}>
-            <footer className='footerContainer'>
-                <div className="containerFooter">
-                    <div className="footerContainer1">
-                        <img src="/logo192.png" alt="logo" width={'200px'} />
-                        <p>
-                            Her Initiative, a young women driven non-profit, aims to redefine
-                            societal norms in Tanzania, promoting financial resilience
-                            among adolescent girls and young women by integrating economic
-                            empowerment and technology.
-                        </p>
-                    </div>
-                    <div className='footerContainer2'>
-                        <h3><b>Quick Links</b></h3>
-                        <p onClick={() => handleClick('/about')}><b>About Us</b></p>
-                        <p onClick={() => handleClick('/news')}><b>Blog</b></p>
-                        <p onClick={() => handleClick('/what')}><b>Projects</b></p>
-                        <p onClick={() => handleClick('/reports')}><b>Reports</b></p>
-                        <p onClick={() => handleClick('/admins')}><b>Admin</b></p>
-                    </div>
-                    <div className='footerContainer4'>
-                        <h3>Get In Touch</h3>
-                        <p>
-                            <b>Physical Address:</b> Asmara Street, Mikocheni,<br /> Dar es Salaam, Tanzania<br />
-                            <b>Postal Address:</b> P.O.Box 66, <br />
-                            <b>Tel:</b> +255 (0) 734283347. <br />
-                            <b>Email:</b> info@herinitiative.or.tz <br />
-                            <b>Web:</b> www.herinitiative.co.tz <br />
-                        </p>
+            <footer className='footerContainer py-2'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3 footerContainer1 mb-4">
+                            <img src="/logo192.png" alt="logo" width="200px" />
+                            <p>
+                                Her Initiative, a young women driven non-profit, aims to redefine
+                                societal norms in Tanzania, promoting financial resilience
+                                among adolescent girls and young women by integrating economic
+                                empowerment and technology.
+                            </p>
+                        </div>
+                        <div className='col-md-3 footerContainer2 mb-4'>
+                            <h3><b>Quick Links</b></h3>
+                            <ul className="list-unstyled">
+                                <li onClick={() => handleClick('/about')}><b>About Us</b></li>
+                                <li onClick={() => handleClick('/news')}><b>Blog</b></li>
+                                <li onClick={() => handleClick('/what')}><b>Projects</b></li>
+                                <li onClick={() => handleClick('/reports')}><b>Reports</b></li>
+                                <li onClick={() => handleClick('/admins')}><b>Admin</b></li>
+                            </ul>
+                        </div>
+                        <div className='col-md-3 footerContainer4 mb-4'>
+                            <h3>Get In Touch</h3>
+                            <p>
+                                <b>Physical Address:</b> Asmara Street, Mikocheni,<br /> Dar es Salaam, Tanzania<br />
+                                <b>Postal Address:</b> P.O.Box 66, <br />
+                                <b>Tel:</b> +255 (0) 734283347. <br />
+                                <b>Email:</b> info@herinitiative.or.tz <br />
+                                <b>Web:</b> www.herinitiative.co.tz <br />
+                            </p>
+                        </div>
+                        <div className="col-md-3 footerContainer4 mb-4">
+                            <h3>Newsletter</h3>
+                            <form className="form-inline">
+                                <div className="input-group">
+                                    <input type="email" className="form-control" placeholder="Enter your email" />
+                                    <div className="input-group-append">
+                                        <button className="btn btn-primary" type="submit">Subscribe</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <div className='donation'></div>
+                        </div>
                     </div>
                 </div>
             </footer>
-            <div className='bottomFooter'>
+            <div className='bottomFooter text-center pt-2 mt-5'>
                 <h3 className='line'>© Copyright 2024 | Her Initiative | All Rights Reserved</h3>
-                <div className='socialIcons'>
-                    <img src='/icons/twitter.png' alt='x' width={'20px'} style={{ paddingRight: '20px', paddingLeft: '40px' }} onClick={() => openSocialMedia(socialUrls.twitter)} />
-                    <Instagram onClick={() => openSocialMedia(socialUrls.instagram)} />
-                    <Facebook onClick={() => openSocialMedia(socialUrls.facebook)} />
-                    <LinkedIn onClick={() => openSocialMedia(socialUrls.linkedin)} />
+                <div className='socialIcons d-inline-flex'>
+                    <img src='/icons/twitter.png' alt='x' width='20px' className="mx-2" onClick={() => openSocialMedia(socialUrls.twitter)} />
+                    <Instagram className="mx-2" onClick={() => openSocialMedia(socialUrls.instagram)} />
+                    <Facebook className="mx-2" onClick={() => openSocialMedia(socialUrls.facebook)} />
+                    <LinkedIn className="mx-2" onClick={() => openSocialMedia(socialUrls.linkedin)} />
                 </div>
             </div>
         </div>
