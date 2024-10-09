2a52060f36df6cc70b70c1ab11d6e58d6b6986b1
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-09 23:48:21 +0300

    deleted page module.css

--- src/screens/home/impact.js
+++ src/screens/home/impact.js
@@ -6,10 +6,12 @@ import { useEffect, useState } from 'react';
 function Impact() {
 
     const [counters, setCounters] = useState([
-        {  count: 0, targetCount: 15112, label: "young women were directly reached, with an additional 25,000,000 young women indirectly reached." },
-        {  count: 0, targetCount: 2805, label: "young women entrepreneurs adopted digital operations, with 210 starting new businesses." },
-        {  count: 0, targetCount: 5270, label: "young women accessed skills and opportunities through the Panda Digital website and SMS services" },
-        {  count: 0, targetCount: 18, label: "youth-led organizations received support, empowering 145 youth with employability skills." }
+        { count: 0, targetCount: 19044, label: "young women were directly reached, with an additional 25,000,000 young women indirectly reached." },
+        { count: 0, targetCount: 3330, label: "young women entrepreneurs adopted digital operations, with 210 starting new businesses." },
+        { count: 0, targetCount: 8385, label: "young women accessed skills and opportunities through the Panda Digital website and SMS services" },
+        { count: 0, targetCount: 38, label: "youth-led organizations received support, empowering 145 youth with employability skills." },
+        { count: 0, targetCount: 250, label: "adolescent girls from 5 Mshiko clubs in 5 Secondary schools gained financial literacy, entrepreneurial, and life skills through tailored training sessions" },
+        { count: 0, targetCount: 108, label: "out-of-school girls gained economic independence, confidence in addressing GBV, and leadership skills." }
     ]);
 
     useEffect(() => {
@@ -31,7 +33,7 @@ function Impact() {
 
         animateCounters();
     }, []);
-    
+
     return (
         <div className='impact'>
             <div className="impactHeader">
@@ -47,7 +49,7 @@ function Impact() {
                                 <Avatar sx={{ bgcolor: '#f3ec1a', marginRight: '0px', marginLeft: '30px', marginTop: '20px' }}>
                                     <Add sx={{ color: '#000000' }} />
                                 </Avatar>
-                                <p><span style={{ color: '#f3ec1a', fontWeight: '700', fontSize: '20px' }}>{impact.count}</span> {impact.label}</p>
+                                <p><span style={{ color: '#f3ec1a', fontWeight: '700', fontSize: '15px' }}>{impact.count}</span> {impact.label}</p>
                             </div>
                         ))}
                     </div>
